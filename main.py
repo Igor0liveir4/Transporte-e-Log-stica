03204d6a0ce8740c601ffc035ac7302cf4407117
Igor <igoroliveirapupo3@gmail.com>
2026-04-06 16:16:26 -0300

    criado os bancos "Motorista" e "Viagens" ✔

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import declarative_base , sessionmaker, relationship
 
 Base = declarative_base()
+
+#Criando a banco "Motorista"
+class Motorista(Base):
+    __tablename__ = "motoristas"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    matricula_funcionario = Column(Integer, nullable=False)
+    status = Column(String(100), nullable=False) 
+
+    #Função de imprimir
+    def __repr__(self):
+        return f"Motorista ID = {self.id} - Nome =  {self.nome} - Matricula do motorista {self.matricula_funcionario} - status = {self.status}"
+
+class Viagem(Base):
+    __tablename__ = "viagens"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    destino_principal = Column(String(100), nullable=False)
+    data_inicio = Column(Date, nullable=False)
+    data_fim = Column(Date, nullable=False)
+
+    def _repr__(self):
+        return f"viagens ID = {self.id} - Destino = {self.destino_principal} - Ida = {self.data_inicio} - Volta = {self.data_fim}"
+    
+
+engine = create_engine("sqlite:///transporte.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine) 
