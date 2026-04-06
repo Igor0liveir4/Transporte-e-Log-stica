573775a24592447bdaf26ad5fa9fe89d463ccd35
Igor <igoroliveirapupo3@gmail.com>
2026-04-06 16:55:58 -0300

    add as funções de add no banco ✔

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Motorista(Base):
     status = Column(String(100), nullable=False) 
 
     #Relacionamento
-    motorista = relationship("Motorista", back_populates="viagens")
+    viagem = relationship("Viagem", back_populates="motorista")
 
     #Função de imprimir
     def __repr__(self):
@@ -31,7 +31,7 @@ class Viagem(Base):
     motorista_id = Column(Integer, ForeignKey("motoristas.id"))
 
     #Relacionamento
-    motorista = relationship("Motorista", back_populates="viagens")
+    motorista = relationship("Motorista", back_populates="viagem")
 
     def _repr__(self):
         return f"viagens ID = {self.id} - Destino = {self.destino_principal} - Ida = {self.data_inicio} - Volta = {self.data_fim}"
@@ -40,3 +40,48 @@ class Viagem(Base):
 engine = create_engine("sqlite:///transporte.db")
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine) 
+
+def cadastrar_motorista():
+    nome_motorista = input("Digite o nome do Motorista: ").strip().capitalize()
+    matricula = input("Digite sua matricula: ").capitalize()
+    status = input("Digite seu status( Ativo, Inativo, Férias, Suspenso.): ").capitalize()
+
+    with Session() as session:
+        try:
+            motorista = Motorista(nome = nome_motorista)
+            
+            session.add(motorista)
+            session.commit()
+            print("Motorista cadastrado com sucesso!")
+
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
+
+cadastrar_motorista()
+
+
+
+
+
+
+
+
+
+
+
+
+def cadastrar_viagens():
+    destino = input("Digite o nome da viagem: ").capitalize()
+    destino_inicio = input("Digite a data do inicio da viagem: ")
+    destino_fim = input("Digite a data de volta: ")
+    with Session() as session:
+        try:
+            viagem = Viagem(destino_principal = destino,data_inicio = destino_inicio, data_fim = destino_fim)
+            session.add(viagem)
+            session.commit()
+            print("Funcionario cadastrado com sucesso!")
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
+
